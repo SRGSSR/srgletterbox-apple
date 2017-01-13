aaff8e9a5985aa7a53318c2b040da5cd57f87286
Samuel Défago <defagos@gmail.com>
2017-01-13 14:06:08 +0100

    Add test target

---
+++ Tests/Sources/SRGLetterboxServiceTests.m
@@ -0,0 +1,18 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SRGLetterboxServiceTestCase : XCTestCase
+
+@end
+
+@implementation SRGLetterboxServiceTestCase
+
+- (void)testDummy
+{}
+
+@end
