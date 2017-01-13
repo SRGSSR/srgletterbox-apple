07c86d99dc1ceb1b3e05fd592daf3fc957c01cad
Samuel Défago <defagos@gmail.com>
2017-01-13 11:27:37 +0100

    Create Xcode project

---
+++ Framework/Sources/SRGLetterbox.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SRGLetterbox.
+FOUNDATION_EXPORT double SRGLetterboxVersionNumber;
+
+//! Project version string for SRGLetterbox.
+FOUNDATION_EXPORT const unsigned char SRGLetterboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRGLetterbox/PublicHeader.h>
