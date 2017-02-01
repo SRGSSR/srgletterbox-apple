f826c1333dd81d4729d6136fdcdeb911704804d2
Samuel Défago <defagos@gmail.com>
2017-02-01 18:56:45 +0100

    Reduce public interface

--- Framework/Sources/Service/SRGLetterboxController+Private.h
+++ Framework/Sources/Service/SRGLetterboxController+Private.h
@@ -4,12 +4,57 @@
 //  License information is available from the LICENSE file.
 //
 
-#import "SRGLetterboxCOntroller.h"
+#import "SRGLetterboxController.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ *  Standard seek intervals
+ */
+OBJC_EXTERN const NSInteger SRGLetterboxBackwardSeekInterval;
+OBJC_EXTERN const NSInteger SRGLetterboxForwardSeekInterval;
+
+/**
+ *  Interface for internal use
+ */
 @interface SRGLetterboxController (Private)
 
+/**
+ *  The media player controller managing playback
+ */
 @property (nonatomic, readonly) SRGMediaPlayerController *mediaPlayerController;
+
+/**
+ *  Return YES iff the player can seek backward from a standard amount of seconds
+ *
+ *  @discussion Always possible for on-demand and DVR streams
+ */
+- (BOOL)canSeekBackward;
+
+/**
+ *  Return YES iff the player can seek forward from a standard amount of seconds
+ *
+ *  @discussion For on-demand and streams, only possible if seeking wouldn't jump past the end. For DVR streams,
+ *              possible until the stream is played live
+ */
+- (BOOL)canSeekForward;
+
+/**
+ *  Seek backward from a standard amount of seconds
+ *
+ *  @discussion If seeking is not possible or if a seek is interrupted, the completion handler will be called with
+ *              finished set to `NO`
+ */
+- (void)seekBackwardWithCompletionHandler:(nullable void (^)(BOOL finished))completionHandler;
+
+/**
+ *  Seek forward from a standard amount of seconds
+ *
+ *  @discussion If seeking is not possible or if a seek is interrupted, the completion handler will be called with
+ *              finished set to `NO`
+ */
+- (void)seekForwardWithCompletionHandler:(nullable void (^)(BOOL finished))completionHandler;
 
 @end
 
-
+NS_ASSUME_NONNULL_END
--- Framework/Sources/Service/SRGLetterboxController.h
+++ Framework/Sources/Service/SRGLetterboxController.h
@@ -9,12 +9,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- *  Standard seek intervals
- */
-OBJC_EXTERN const NSInteger SRGLetterboxBackwardSeekInterval;
-OBJC_EXTERN const NSInteger SRGLetterboxForwardSeekInterval;
-
-/**
  *  Notification sent when playback metadata is updated (use the dictionary keys below to get previous and new values)
  */
 OBJC_EXTERN NSString * const SRGLetterboxMetadataDidChangeNotification;
@@ -64,37 +58,6 @@ OBJC_EXTERN NSString * const SRGLetterboxPlaybackDidFailNotification;
  *  Reset playback, stopping a playback request if any has been made
  */
 - (void)reset;
-
-/**
- *  Return YES iff the player can seek backward from a standard amount of seconds
- *
- *  @discussion Always possible for on-demand and DVR streams
- */
-- (BOOL)canSeekBackward;
-
-/**
- *  Return YES iff the player can seek forward from a standard amount of seconds
- *
- *  @discussion For on-demand and streams, only possible if seeking wouldn't jump past the end. For DVR streams,
- *              possible until the stream is played live
- */
-- (BOOL)canSeekForward;
-
-/**
- *  Seek backward from a standard amount of seconds
- *
- *  @discussion If seeking is not possible or if a seek is interrupted, the completion handler will be called with 
- *              finished set to `NO`
- */
-- (void)seekBackwardWithCompletionHandler:(nullable void (^)(BOOL finished))completionHandler;
-
-/**
- *  Seek forward from a standard amount of seconds
- *
- *  @discussion If seeking is not possible or if a seek is interrupted, the completion handler will be called with
- *              finished set to `NO`
- */
-- (void)seekForwardWithCompletionHandler:(nullable void (^)(BOOL finished))completionHandler;
 
 @end
 
