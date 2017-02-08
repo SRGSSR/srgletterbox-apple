8bf5b356fd00dbc327420d37c9a56cc61e0c53a9
Samuel Défago <defagos@gmail.com>
2017-02-08 18:27:45 +0100

    Add quick player switching

--- Demo/Sources/Demos/MultiPlayerViewController.m
+++ Demo/Sources/Demos/MultiPlayerViewController.m
@@ -43,6 +43,15 @@
     
     self.smallLetterboxView1.controller = self.smallLetterboxController1;
     self.smallLetterboxView2.controller = self.smallLetterboxController2;
+    
+    [self.smallLetterboxView1 setUserInterfaceHidden:YES animated:NO togglable:NO];
+    [self.smallLetterboxView2 setUserInterfaceHidden:YES animated:NO togglable:NO];
+    
+    UIGestureRecognizer *tapGestureRecognizer1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(switchToStream1:)];
+    [self.smallLetterboxView1 addGestureRecognizer:tapGestureRecognizer1];
+    
+    UIGestureRecognizer *tapGestureRecognizer2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(switchToStream2:)];
+    [self.smallLetterboxView2 addGestureRecognizer:tapGestureRecognizer2];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -70,6 +79,30 @@
             [[SRGLetterboxService sharedService].controller reset];
         }
     }
+}
+
+#pragma mark Gesture recognizers
+
+- (void)switchToStream1:(UIGestureRecognizer *)gestureRecognizer
+{
+    SRGLetterboxController *letterboxController = [SRGLetterboxService sharedService].controller;
+    
+    self.letterboxView.controller = self.smallLetterboxController1;
+    [SRGLetterboxService sharedService].controller = self.smallLetterboxController1;
+    
+    self.smallLetterboxView1.controller = letterboxController;
+    self.smallLetterboxController1 = letterboxController;
+}
+
+- (void)switchToStream2:(UIGestureRecognizer *)gestureRecognizer
+{
+    SRGLetterboxController *letterboxController = [SRGLetterboxService sharedService].controller;
+    
+    self.letterboxView.controller = self.smallLetterboxController2;
+    [SRGLetterboxService sharedService].controller = self.smallLetterboxController2;
+    
+    self.smallLetterboxView2.controller = letterboxController;
+    self.smallLetterboxController2 = letterboxController;
 }
 
 @end
