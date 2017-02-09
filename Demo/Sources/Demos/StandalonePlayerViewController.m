0dfc14700bd937dd76dccd15a757ced3a7e5250f
Samuel Défago <defagos@gmail.com>
2017-02-09 16:20:45 +0100

    Don’t enable standalone player as service by default

--- Demo/Sources/Demos/StandalonePlayerViewController.m
+++ Demo/Sources/Demos/StandalonePlayerViewController.m
@@ -37,14 +37,6 @@
 
 #pragma mark View lifecycle
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    [SRGLetterboxController enableBackgroundServicesWithController:self.letterboxController
-                                          pictureInPictureDelegate:nil];
-}
-
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
