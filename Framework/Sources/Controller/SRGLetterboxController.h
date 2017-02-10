cbcc7da9346447778def7cc70e120eee2678cc19
Samuel Défago <defagos@gmail.com>
2017-02-10 19:05:13 +0100

    Polish documentation

--- Framework/Sources/Controller/SRGLetterboxController.h
+++ Framework/Sources/Controller/SRGLetterboxController.h
@@ -41,49 +41,51 @@ OBJC_EXTERN NSString * const SRGLetterboxErrorKey;
  *  Letterbox media player controller, managing playback, as well as automatic metadata retrieval. Applications
  *  can use the metadata available from this controller to display additional playback information (e.g. title
  *  or description of the content). The controller can be used in isolation for playback without display, but
- *  is usually best used bound to a Letterbox view
+ *  is usually best used bound to a Letterbox view. A Letterbox controlller is not enabled for common
+ *  application-wide features by default (e.g. Airplay or picture in picture). To enable such features, @see
+ *  `SRGLetterboxService`.
  */
 @interface SRGLetterboxController : NSObject
 
 /**
- *  Play the specified Uniform Resource Name
+ *  Play the specified URN (Uniform Resource Name).
  *
- *  @discussion Does nothing if the urn is the one currently being played. The best available quality is automatically
- *              played
+ *  @discussion Does nothing if the URN is the one currently being played. The best available quality is automatically
+ *              played.
  */
 - (void)playURN:(SRGMediaURN *)URN;
 
 /**
- *  Play the specified media
+ *  Play the specified media.
  *
- *  @discussion Does nothing if the urn is the one currently being played. The best available quality is automatically
- *              played
+ *  @discussion Does nothing if the URN is the one currently being played. The best available quality is automatically
+ *              played.
  */
 - (void)playMedia:(SRGMedia *)media;
 
 /**
- *  Play the specified Uniform Resource Name
+ *  Play the specified URN (Uniform Resource Name).
  *
- *  @discussion Does nothing if the urn is the one currently being played. If the preferred quality is set to
- *              `SRGQualityNone`, the best available quality will be automatically played
+ *  @discussion Does nothing if the URN is the one currently being played. If the preferred quality is set to
+ *              `SRGQualityNone`, the best available quality will be automatically played.
  */
 - (void)playURN:(SRGMediaURN *)URN withPreferredQuality:(SRGQuality)preferredQuality;
 
 /**
- *  Play the specified media
+ *  Play the specified media.
  *
- *  @discussion Does nothing if the urn is the one currently being played. If the preferred quality is set to
- *              `SRGQualityNone`, the best available quality will be automatically played
+ *  @discussion Does nothing if the media is the one currently being played. If the preferred quality is set to
+ *              `SRGQualityNone`, the best available quality will be automatically played.
  */
 - (void)playMedia:(SRGMedia *)media withPreferredQuality:(SRGQuality)preferredQuality;
 
 /**
- *  Reset playback, stopping a playback request if any has been made
+ *  Reset playback, stopping a playback request if any has been made.
  */
 - (void)reset;
 
 /**
- *  Set to YES to mute the player. Default is NO
+ *  Set to YES to mute the player. Default is NO.
  */
 @property (nonatomic, getter=isMuted) BOOL muted;
 
@@ -91,27 +93,27 @@ OBJC_EXTERN NSString * const SRGLetterboxErrorKey;
 
 /**
  *  Playback information. Changes are notified through `SRGLetterboxMetadataDidChangeNotification` and
- *  `SRGLetterboxPlaybackDidFailNotification`
+ *  `SRGLetterboxPlaybackDidFailNotification`.
  */
 @interface SRGLetterboxController (PlaybackInformation)
 
 /**
- *  URN
+ *  Unified Resource Name being played.
  */
 @property (nonatomic, readonly, nullable) SRGMediaURN *URN;
 
 /**
- *  Media information
+ *  Media information.
  */
 @property (nonatomic, readonly, nullable) SRGMedia *media;
 
 /**
- *  Media composition
+ *  Media composition.
  */
 @property (nonatomic, readonly, nullable) SRGMediaComposition *mediaComposition;
 
 /**
- *  Error if any has been encountered
+ *  Error (if any has been encountered).
  */
 @property (nonatomic, readonly) NSError *error;
 
@@ -120,17 +122,17 @@ OBJC_EXTERN NSString * const SRGLetterboxErrorKey;
 @interface SRGLetterboxController (BackgroundServices)
 
 /**
- *  Return YES iff the receiver is enabled for background services
+ *  Return `YES` iff the receiver is enabled for background services.
  */
 @property (nonatomic, readonly, getter=areBackgroundServicesEnabled) BOOL backgroundServicesEnabled;
 
 /**
- *  Return YES iff the receiver is enabled for picture in picture
+ *  Return `YES` iff the receiver is enabled for picture in picture.
  */
 @property (nonatomic, readonly, getter=isPictureInPictureEnabled) BOOL pictureInPictureEnabled;
 
 /**
- *  Return YES iff picture in picture is active for the receiver
+ *  Return `YES` iff picture in picture is active for the receiver.
  */
 @property (nonatomic, readonly, getter=isPictureInPictureActive) BOOL pictureInPictureActive;
 
--- Framework/Sources/Service/SRGLetterboxService+Private.h
+++ Framework/Sources/Service/SRGLetterboxService+Private.h
@@ -8,12 +8,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ *  Notification sent when the service settings have been updated.
+ */
 OBJC_EXTERN NSString * const SRGLetterboxServiceSettingsDidChangeNotification;
 
 @interface SRGLetterboxService (Private)
 
 /**
- *  Stop picture in picture. If `restoreUserInterface` is set to NO, the restoration delegate methods will not be called
+ *  Stop picture in picture. If `restoreUserInterface` is set to `NO`, the restoration delegate methods will not be called
  */
 - (void)stopPictureInPictureRestoreUserInterface:(BOOL)restoreUserInterface;
 
