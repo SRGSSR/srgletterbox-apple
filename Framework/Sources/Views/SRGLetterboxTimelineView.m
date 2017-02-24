011f044e5ec0d1f854e3e5ff874941def989fbea
Samuel Défago <defagos@gmail.com>
2017-02-24 15:28:45 +0100

    Consolidate segments

--- Framework/Sources/Views/SRGLetterboxTimelineView.m
+++ Framework/Sources/Views/SRGLetterboxTimelineView.m
@@ -17,6 +17,8 @@ static void commonInit(SRGLetterboxTimelineView *self);
 
 @property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
 
+@property (nonatomic) NSArray<SRGSegment *> *segments;
+
 @end
 
 @implementation SRGLetterboxTimelineView
@@ -104,6 +106,20 @@ static void commonInit(SRGLetterboxTimelineView *self);
 
 - (void)reloadData
 {
+    NSMutableArray<SRGSegment *> *segments = [NSMutableArray array];
+    
+    // Show logical segments for the current chapter (if any), and display other chapters but not expanded
+    SRGMediaComposition *mediaComposition = self.controller.mediaComposition;
+    for (SRGChapter *chapter in mediaComposition.chapters) {
+        if (chapter == mediaComposition.mainChapter && chapter.segments.count != 0) {
+            [segments addObjectsFromArray:chapter.segments];
+        }
+        else {
+            [segments addObject:chapter];
+        }
+    }
+    self.segments = [segments copy];
+    
     [self.collectionView reloadData];
 }
 
@@ -111,13 +127,13 @@ static void commonInit(SRGLetterboxTimelineView *self);
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.controller.mediaComposition.chapters.count;
+    return self.segments.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     SRGLetterboxSegmentCell *segmentCell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([SRGLetterboxSegmentCell class]) forIndexPath:indexPath];
-    segmentCell.segment = self.controller.mediaComposition.chapters[indexPath.row];
+    segmentCell.segment = self.segments[indexPath.row];
     return segmentCell;
 }
 
