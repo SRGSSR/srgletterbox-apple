4e374706dfcb58722f8255f70b471159f8acff34
Samuel Défago <defagos@gmail.com>
2017-02-28 08:41:46 +0100

    Fix behavior for chapters

--- Framework/Sources/Views/SRGLetterboxTimelineView.m
+++ Framework/Sources/Views/SRGLetterboxTimelineView.m
@@ -91,7 +91,9 @@ static void commonInit(SRGLetterboxTimelineView *self);
 {
     // Try to locate a segment whose parent is the current segment (if any) and matching the specified time
     NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(SRGSegment * _Nonnull segment, NSDictionary<NSString *, id> *_Nullable bindings) {
-        return [segment.fullLengthURN isEqual:currentSegment.URN] && segment.markIn / 1000. <= timeInSeconds && timeInSeconds <= segment.markOut / 1000.;
+        return ! [segment isKindOfClass:[SRGChapter class]]
+                  && [segment.fullLengthURN isEqual:currentSegment.URN]
+                  && segment.markIn / 1000. <= timeInSeconds && timeInSeconds <= segment.markOut / 1000.;
     }];
     
     // Use the current segment as fallback
