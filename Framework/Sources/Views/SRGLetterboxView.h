2bd7c76c5b2b4c6c9ef3d1bcc3fd0bee24d0dc7e
Samuel Défago <defagos@gmail.com>
2017-03-08 14:42:44 +0100

    Improve documentation

--- Framework/Sources/Views/SRGLetterboxView.h
+++ Framework/Sources/Views/SRGLetterboxView.h
@@ -179,7 +179,7 @@ IB_DESIGNABLE
 @property (nonatomic, readonly, getter=isUserInterfaceTogglable) BOOL userInterfaceTogglable;
 
 /**
- *  Change the user interface controls visibility.
+ *  Change the user interface controls visibility. Togglability is not altered.
  *
  *  @param hidden Whether the user interface must be hidden.
  *  @param animated Whether the transition must be animated.
