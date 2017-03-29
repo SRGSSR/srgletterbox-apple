382a97894123830180b2b51f842a252ed03dbbb2
Samuel Défago <defagos@gmail.com>
2017-03-29 20:21:23 +0200

    Remove now unused protocols

--- Demo/Sources/Demos/ModalPlayerViewController.h
+++ Demo/Sources/Demos/ModalPlayerViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ModalPlayerViewController : UIViewController <SRGLetterboxPictureInPictureDelegate, SRGLetterboxViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
+@interface ModalPlayerViewController : UIViewController <SRGLetterboxPictureInPictureDelegate, SRGLetterboxViewDelegate>
 
 - (instancetype)initWithURN:(nullable SRGMediaURN *)URN;
 
