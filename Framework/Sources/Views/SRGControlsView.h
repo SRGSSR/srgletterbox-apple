5688636c13889674ef578452f8770f9a881be081
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2017-03-31 16:31:14 +0200

    Missing files

---
+++ Framework/Sources/Views/SRGControlsView.h
@@ -0,0 +1,38 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class SRGControlsView;
+
+/**
+ *  Controls view delegate protocol gets the layoutSubviews event
+ */
+@protocol SRGControlsViewDelegate <NSObject>
+
+- (void)controlsViewDidLayoutSubviews:(SRGControlsView *)controlsView;
+
+@end
+
+/**
+ *  This simple view has just a delegate to forward layoutSubviews events. Control buttons aren't here.
+ *
+ *  @discussion See `SRGLetterboxView` to see all control buttons
+ */
+
+@interface SRGControlsView : UIView
+
+/**
+ *  View optional delegate.
+ */
+@property (nonatomic, weak, nullable) IBOutlet id<SRGControlsViewDelegate> delegate;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
