6601668105e0a565141752fde1e8d2179e3668f7
Samuel Défago <defagos@gmail.com>
2017-05-31 13:38:41 +0200

    Avoid potential unintentional method name overrides

--- Framework/Sources/Helpers/UIImage+SRGLetterbox.m
+++ Framework/Sources/Helpers/UIImage+SRGLetterbox.m
@@ -10,13 +10,13 @@
 
 // ** Private SRGDataProvider fixes for Play. See NSURL+SRGDataProvider.h for more information
 
-@interface NSURL (SRGLetterbox_Private_SRGDataProvider)
+@interface NSURL (srgletterbox_containsDatePrivate_SRGDataProvider)
 
 - (NSURL *)srg_URLForDimension:(SRGImageDimension)dimension withValue:(CGFloat)value uid:(nullable NSString *)uid type:(nullable NSString *)type;
 
 @end
 
-@interface NSObject (SRGLetterbox_Private_SRGDataProvider)
+@interface NSObject (srgletterbox_containsDatePrivate_SRGDataProvider)
 
 // Declare internal image URL accessor
 @property (nonatomic, readonly) NSURL *imageURL;
--- Framework/Sources/Views/SRGLetterboxView.m
+++ Framework/Sources/Views/SRGLetterboxView.m
@@ -561,7 +561,7 @@ static void commonInit(SRGLetterboxView *self);
         
         // Display program artwork (if any) when the slider position is within the current program, otherwise channel artwork.
         NSDate *sliderDate = [NSDate dateWithTimeIntervalSinceNow:self.timeSlider.value - self.timeSlider.maximumValue];
-        if ([channel.currentProgram containsDate:sliderDate]) {
+        if ([channel.currentProgram srgletterbox_containsDate:sliderDate]) {
             if (! [self.imageView srg_requestImageForObject:channel.currentProgram withScale:SRGImageScaleLarge]) {
                 [self.imageView srg_requestImageForObject:channel withScale:SRGImageScaleLarge];
             }
