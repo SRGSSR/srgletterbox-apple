f89e2be73299ac69ef56ec3a165b8f825dcc171f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2017-08-24 10:31:58 +0200

    Remove unused methods on NSDateFormatter

--- Framework/Sources/Helpers/NSDateFormatter+SRGLetterbox.h
+++ Framework/Sources/Helpers/NSDateFormatter+SRGLetterbox.h
@@ -16,29 +16,9 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSDateFormatter *)srg_relativeDateAndTimeFormatter;
 
 /**
- *  Use only relative date formatting, i.e. displays today / yesterday for dates near today
- */
-+ (NSDateFormatter *)srg_relativeDateFormatter;
-
-/**
- *  Use only short time formatting
- */
-+ (NSDateFormatter *)srg_relativeTimeFormatter;
-
-/**
  *  Same as `-srg_relativeDateAndTimeFormatter` but for accessibility
  */
 + (NSDateFormatter *)srg_relativeDateAndTimeAccessibilityFormatter;
-
-/**
- *  Same as `-srg_relativeDateAndTimeFormatter` but for accessibility
- */
-+ (NSDateFormatter *)srg_relativeDateAccessibilityFormatter;
-
-/**
- *  Same as `-srg_relativeDateAndTimeFormatter` but for accessibility
- */
-+ (NSDateFormatter *)srg_relativeTimeAccessibilityFormatter;
 
 @end
 
--- Framework/Sources/Helpers/NSDateFormatter+SRGLetterbox.m
+++ Framework/Sources/Helpers/NSDateFormatter+SRGLetterbox.m
@@ -21,32 +21,6 @@
     return s_dateFormatter;
 }
 
-+ (NSDateFormatter *)srg_relativeDateFormatter
-{
-    static NSDateFormatter *s_dateFormatter;
-    static dispatch_once_t s_onceToken;
-    dispatch_once(&s_onceToken, ^{
-        s_dateFormatter = [[NSDateFormatter alloc] init];
-        s_dateFormatter.dateStyle = NSDateFormatterShortStyle;
-        s_dateFormatter.timeStyle = NSDateFormatterNoStyle;
-        s_dateFormatter.doesRelativeDateFormatting = YES;
-    });
-    return s_dateFormatter;
-}
-
-+ (NSDateFormatter *)srg_relativeTimeFormatter
-{
-    static NSDateFormatter *s_dateFormatter;
-    static dispatch_once_t s_onceToken;
-    dispatch_once(&s_onceToken, ^{
-        s_dateFormatter = [[NSDateFormatter alloc] init];
-        s_dateFormatter.dateStyle = NSDateFormatterNoStyle;
-        s_dateFormatter.timeStyle = NSDateFormatterShortStyle;
-        s_dateFormatter.doesRelativeDateFormatting = YES;
-    });
-    return s_dateFormatter;
-}
-
 + (NSDateFormatter *)srg_relativeDateAndTimeAccessibilityFormatter
 {
     static NSDateFormatter *s_dateFormatter;
@@ -54,32 +28,6 @@
     dispatch_once(&s_onceToken, ^{
         s_dateFormatter = [[NSDateFormatter alloc] init];
         s_dateFormatter.dateStyle = NSDateFormatterFullStyle;
-        s_dateFormatter.timeStyle = NSDateFormatterFullStyle;
-        s_dateFormatter.doesRelativeDateFormatting = YES;
-    });
-    return s_dateFormatter;
-}
-
-+ (NSDateFormatter *)srg_relativeDateAccessibilityFormatter
-{
-    static NSDateFormatter *s_dateFormatter;
-    static dispatch_once_t s_onceToken;
-    dispatch_once(&s_onceToken, ^{
-        s_dateFormatter = [[NSDateFormatter alloc] init];
-        s_dateFormatter.dateStyle = NSDateFormatterFullStyle;
-        s_dateFormatter.timeStyle = NSDateFormatterNoStyle;
-        s_dateFormatter.doesRelativeDateFormatting = YES;
-    });
-    return s_dateFormatter;
-}
-
-+ (NSDateFormatter *)srg_relativeTimeAccessibilityFormatter
-{
-    static NSDateFormatter *s_dateFormatter;
-    static dispatch_once_t s_onceToken;
-    dispatch_once(&s_onceToken, ^{
-        s_dateFormatter = [[NSDateFormatter alloc] init];
-        s_dateFormatter.dateStyle = NSDateFormatterNoStyle;
         s_dateFormatter.timeStyle = NSDateFormatterFullStyle;
         s_dateFormatter.doesRelativeDateFormatting = YES;
     });
