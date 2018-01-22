e20ccb32b1b7ce8213803b09b6a508a38085a5ce
Samuel Défago <defagos@gmail.com>
2018-01-22 13:17:59 +0100

    Add 360 chapter example in demo

--- Demo/Sources/Demos/DemosViewController.m
+++ Demo/Sources/Demos/DemosViewController.m
@@ -151,6 +151,7 @@
     static NSString * const kVideoOnDemandBlockedSegmentOverlapURNString = @"urn:srf:video:d57f5c1c-080f-49a2-864e-4a1a83e41ae1";
     static NSString * const kVideoOnDemandHybridURNString = @"urn:rts:audio:8581974";
     static NSString * const kVideoOnDemand360URNString = @"urn:rts:video:8414077";
+    static NSString * const kVideoOnDemandWithChapters360URNString = @"urn:rts:video:7800215";
     static NSString * const kVideoOnDemandNoTokenURNString = @"urn:srf:video:db741834-044f-443e-901a-e2fc03a4ef25";
     
     static NSString * const kVideoOnDemandChaptersOnlyFullLengthURNString = @"urn:srf:video:dc4a4f8c-e83e-46b3-a5e3-ebfde3a29b88";
@@ -282,51 +283,56 @@
                 }
                     
                 case 9: {
-                    [self openModalPlayerWithURNString:kVideoOnDemandNoTokenURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kVideoOnDemandWithChapters360URNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 10: {
-                    [self openModalPlayerWithURNString:kVideoDVRURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kVideoOnDemandNoTokenURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 11: {
-                    [self openModalPlayerWithURNString:kVideoLiveURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kVideoDVRURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 12: {
-                    [self openModalPlayerWithURNString:kAudioOnDemandSegmentsURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kVideoLiveURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 13: {
-                    [self openModalPlayerWithURNString:kAudioOnDemandStartOnSegmentURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kAudioOnDemandSegmentsURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 14: {
-                    [self openModalPlayerWithURNString:kAudioDVRURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kAudioOnDemandStartOnSegmentURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 15: {
-                    [self openModalPlayerWithURNString:kAudioDVRRegionalURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kAudioDVRURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 16: {
-                    [self openModalPlayerWithURNString:kInvalidURNString chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kAudioDVRRegionalURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 17: {
-                    [self openModalPlayerWithURNString:nil chaptersOnly:NO];
+                    [self openModalPlayerWithURNString:kInvalidURNString chaptersOnly:NO];
                     break;
                 }
                     
                 case 18: {
+                    [self openModalPlayerWithURNString:nil chaptersOnly:NO];
+                    break;
+                }
+                    
+                case 19: {
                     [tableView deselectRowAtIndexPath:indexPath animated:YES];
                     [self openCustomURNEntryAlertWithCompletionBlock:^(NSString * _Nullable URNString) {
                         [self openModalPlayerWithURNString:URNString chaptersOnly:NO];
