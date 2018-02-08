d0290bf96f3ec6ccbb3f523065bce6d32dcbd7ee
Samuel Défago <defagos@gmail.com>
2018-02-08 10:38:26 +0100

    Add continuous playback view

---
+++ Framework/Sources/Views/SRGContinuousPlaybackView.h
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+IB_DESIGNABLE
+@interface SRGContinuousPlaybackView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Framework/Sources/Views/SRGContinuousPlaybackView.m
@@ -0,0 +1,65 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import "SRGContinuousPlaybackView.h"
+
+#import "NSBundle+SRGLetterbox.h"
+
+#import <Masonry/Masonry.h>
+#import <SRGAppearance/SRGAppearance.h>
+
+static void commonInit(SRGContinuousPlaybackView *self);
+
+@interface SRGContinuousPlaybackView ()
+
+@end
+
+@implementation SRGContinuousPlaybackView
+
+#pragma mark Object lifecycle
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        commonInit(self);
+        
+        // The top-level view loaded from the xib file and initialized in `commonInit` is NOT an SRGContinuousPlaybackView. Manually
+        // calling `-awakeFromNib` forces the final view initialization (also see comments in `commonInit`).
+        [self awakeFromNib];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        commonInit(self);
+    }
+    return self;
+}
+
+#pragma mark Overrides
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    // TODO:
+}
+
+@end
+
+static void commonInit(SRGContinuousPlaybackView *self)
+{
+    // This makes design in a xib and Interface Builder preview (IB_DESIGNABLE) work. The top-level view must NOT be
+    // an SRGCountdownView to avoid infinite recursion
+    UIView *view = [[[NSBundle srg_letterboxBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil] firstObject];
+    view.backgroundColor = [UIColor clearColor];
+    [self addSubview:view];
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self);
+    }];
+}
