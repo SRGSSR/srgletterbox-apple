e0848bc35a252fb2cb73ec1247fbee22665e23fe
Samuel Défago <defagos@gmail.com>
2018-03-08 22:33:56 +0100

    Display AirPlay audio image for audios only

--- Framework/Sources/Views/SRGLetterboxView.m
+++ Framework/Sources/Views/SRGLetterboxView.m
@@ -200,6 +200,7 @@ static void commonInit(SRGLetterboxView *self);
     self.viewModeButton.viewModeMonoscopicImage = [UIImage imageNamed:@"view_mode_monoscopic-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
     self.viewModeButton.viewModeStereoscopicImage = [UIImage imageNamed:@"view_mode_stereoscopic-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
     self.airplayButton.image = [UIImage imageNamed:@"airplay-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
+    self.airplayButton.audioImage = [UIImage imageNamed:@"airplay_audio-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
     self.pictureInPictureButton.startImage = [UIImage imageNamed:@"picture_in_picture_start-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
     self.pictureInPictureButton.stopImage = [UIImage imageNamed:@"picture_in_picture_stop-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
     self.tracksButton.image = [UIImage imageNamed:@"subtitles_off-48" inBundle:[NSBundle srg_letterboxBundle] compatibleWithTraitCollection:nil];
