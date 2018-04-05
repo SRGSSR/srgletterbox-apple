5a1f4a1904b3a38a2f2de56fcfb48446d133c570
Samuel Défago <defagos@gmail.com>
2018-04-05 10:38:08 +0200

    Fix iOS 11.3 livestream issues
    This is a (hopefully) temporary workaround for an Apple bug.

--- Framework/Sources/Controller/SRGLetterboxController.m
+++ Framework/Sources/Controller/SRGLetterboxController.m
@@ -171,6 +171,9 @@ static BOOL SRGLetterboxControllerIsLoading(SRGLetterboxDataAvailability dataAva
     if (self = [super init]) {
         self.mediaPlayerController = [[SRGMediaPlayerController alloc] init];
         
+        // FIXME: See https://github.com/SRGSSR/SRGMediaPlayer-iOS/issues/50. Workaround so that the test passes on iOS 11.3.
+        self.mediaPlayerController.minimumDVRWindowLength = 40.;
+        
         @weakify(self)
         self.mediaPlayerController.playerConfigurationBlock = ^(AVPlayer *player) {
             @strongify(self)
