b07e9ec643d7d5e340bd3962ee2e55adbc6177cd
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2018-04-07 18:08:25 +0200

    Set Letterbox and its version as the analytics player name and version

--- Framework/Sources/Controller/SRGLetterboxController.m
+++ Framework/Sources/Controller/SRGLetterboxController.m
@@ -156,6 +156,8 @@ static BOOL SRGLetterboxControllerIsLoading(SRGLetterboxDataAvailability dataAva
 {
     if (self = [super init]) {
         self.mediaPlayerController = [[SRGMediaPlayerController alloc] init];
+        self.mediaPlayerController.analyticsPlayerName = @"SRGLetterbox";
+        self.mediaPlayerController.analyticsPlayerVersion = [NSBundle srg_letterboxBundle].infoDictionary[@"CFBundleShortVersionString"];
         
         // FIXME: See https://github.com/SRGSSR/SRGMediaPlayer-iOS/issues/50. Workaround so that the test passes on iOS 11.3.
         self.mediaPlayerController.minimumDVRWindowLength = 40.;
