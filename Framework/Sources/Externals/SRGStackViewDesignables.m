f37e059839cb81c7840ab6a156bbb77e4f378cb7
Samuel Défago <defagos@gmail.com>
2018-04-22 19:21:22 +0200

    Add SRGStackView

---
+++ Framework/Sources/Externals/SRGStackViewDesignables.m
@@ -0,0 +1,115 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <SRGStackView/SRGStackView.h>
+
+/**
+ *  File for clients to opt-in for designable support (preview in Interface Builder).
+ *
+ *  Copy this file into your project, do not include it directly or use a symbolic link to it, it won't work.
+ *  Using designables will trigger additional background compilations, this is normal. If your project takes
+ *  too long to compile, you might not want to opt-in for designables.
+ */
+
+IB_DESIGNABLE
+@interface SRGStackView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIActivityIndicatorView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIButton (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUICollectionView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIDatePicker (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIImageView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUILabel (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIPageControl (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIPickerView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIProgressView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIScrollView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUISearchBar (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUISegmentedControl (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUISlider (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIStackView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIStepper (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUISwitch (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUITabBar (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUITableView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUITextField (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUITextView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIToolbar (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIVisualEffectView (Designable)
+@end
+
+IB_DESIGNABLE
+@interface SRGUIWebView (Designable)
+@end
