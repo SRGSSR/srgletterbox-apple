7fe15a1f4348430aab80b5e24972652063332920
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2018-04-26 16:30:33 +0200

    Update Live red label to the official live color

--- Framework/Sources/Views/Overlays/SRGLiveLabel.m
+++ Framework/Sources/Views/Overlays/SRGLiveLabel.m
@@ -63,7 +63,7 @@ static void commonInit(SRGLiveLabel *self);
 
 static void commonInit(SRGLiveLabel *self)
 {
-    self.backgroundColor = [UIColor redColor];
+    self.backgroundColor = [UIColor colorWithRed:213.f / 255.f green:0.f blue:0.f alpha:1.f];
     self.textColor = [UIColor whiteColor];
     self.attributedText = [[NSAttributedString alloc] initWithString:SRGLetterboxLocalizedString(@"Live", @"Very short text in the slider bubble, or in the bottom right corner of the Letterbox view when playing a live only stream or a DVR stream in live").uppercaseString attributes:@{ NSFontAttributeName : [UIFont srg_boldFontWithSize:14.f] }];
     self.textAlignment = NSTextAlignmentCenter;
