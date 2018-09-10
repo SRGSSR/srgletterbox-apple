b6f327b445d4e79b891a864aee93480d16429bb9
Samuel Défago <defagos@gmail.com>
2018-09-10 08:41:11 +0200

    Consolidate diagnostics service setup

--- Framework/Sources/SRGLetterbox.m
+++ Framework/Sources/SRGLetterbox.m
@@ -7,16 +7,34 @@
 #import "SRGLetterbox.h"
 
 #import "NSBundle+SRGLetterbox.h"
+#import "SRGLetterboxLogger.h"
 
 #import <SRGDiagnostics/SRGDiagnostics.h>
 
+/**
+ *  Diagnostics service URL key in bundle information dictionnary.
+ */
+NSString * const SRGLetterboxDiagnosticsServiceURLKey = @"DiagnosticsServiceURL";
+
 __attribute__((constructor)) static void SRGLetterboxDiagnosticsInit(void)
 {
-    SRGDiagnosticsService *diagnosticsService = [SRGDiagnosticsService serviceWithName:@"SRGPlaybackMetrics"];
-    diagnosticsService.submissionBlock = ^(NSDictionary * _Nonnull JSONDictionary, void (^ _Nonnull completionBlock)(BOOL)) {
-        // TODO: Network request
-        NSLog(@"Diagnostics report: %@", JSONDictionary);
-        completionBlock(YES);
+    [SRGDiagnosticsService serviceWithName:@"SRGPlaybackMetrics"].submissionBlock = ^(NSDictionary * _Nonnull JSONDictionary, void (^ _Nonnull completionBlock)(BOOL)) {
+        NSString *diagnosticsServiceURLString = [[NSBundle srg_letterboxBundle] objectForInfoDictionaryKey:SRGLetterboxDiagnosticsServiceURLKey];
+        NSURL *diagnosticsServiceURL = (diagnosticsServiceURLString.length > 0) ? [NSURL URLWithString:diagnosticsServiceURLString] : nil;
+        if (diagnosticsServiceURL) {
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:diagnosticsServiceURL];
+            request.HTTPMethod = @"POST";
+            [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            request.HTTPBody = [NSJSONSerialization dataWithJSONObject:JSONDictionary options:0 error:NULL];
+            [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                SRGLetterboxLogInfo(@"diagnostics", @"SRGPlaybackMetrics report %@: %@", (error != nil) ? @"sent" : @"not sent", JSONDictionary);
+                completionBlock(error != nil);
+            }] resume];
+        }
+        else {
+            SRGLetterboxLogInfo(@"diagnostics", @"SRGPlaybackMetrics report: %@", JSONDictionary);
+            completionBlock(YES);
+        }
     };
 }
 
