0e106a6ac6639e546bbc8b287fa6ef9f77df8d38
Samuel Défago <defagos@gmail.com>
2018-09-10 18:09:25 +0200

    Document report information

--- Framework/Sources/SRGLetterbox.m
+++ Framework/Sources/SRGLetterbox.m
@@ -18,6 +18,53 @@ NSString * const SRGLetterboxDiagnosticsServiceURLKey = @"DiagnosticsServiceURL"
 
 __attribute__((constructor)) static void SRGLetterboxDiagnosticsInit(void)
 {
+    // SRGPlaybackMetrics anatomy:
+    // ---------------------------
+    //                                             ┌─────────────────┐
+    //                                             │                 │
+    //                   ┌─────────────────┐       │  The media URL  │
+    //                   │  The app asks   │       │received from the│                              ┌──────────────────┐
+    //                   │Letterbox to play│       │  IL is played   │                              │  Media playback  │
+    //                   │    the media    │       │                 │                              │      starts      │
+    //                   └─────────────────┘       └─────────────────┘                              └──────────────────┘
+    //                            │                         │                                                 │
+    //   ┌─────────────────┐      │                         │                                                 │
+    //   │ User taps on a  │      │                         │                                                 │
+    //   │      media      │      │                         │    ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─        │
+    //   └─────────────────┘      │                         │                 playerResult            │┌──────┘
+    //            │               │                         │    └ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ │
+    //            └───────┐       │                         │    ┌ ─ ─ ─ ─ ─ ─ ┐                       │
+    //                    │       │   ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐ │      tokenResult                         │
+    //       ─────────────▼───────▼──▲─     ilResult      ──▼───▲┤  drmResult  ├─────▲─────────────────▼──────────────────▶
+    //                    │       │  │└ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘     │ ─ ─ ─ ─ ─ ─ ─      └────┐            │                 time
+    //                               │                          │                         │
+    //                    │       │  │                          │                         │            │
+    //                               └────────────┐             └──────────┐     ┌─────────────────┐
+    //                    │       │               │                        │     │ Media buffering │   │
+    //                                            │                        │     │     starts      │
+    //                    │       │               │                        │     └─────────────────┘   │
+    //                                            │                        │
+    //                    │       │      ┌─────────────────┐       ┌───────────────┐                   │
+    //                                   │  The media is   │       │  Token / DRM  │
+    //                    │       │      │ requested from  │       │keys retrieval │                   │
+    //                                   │     the IL      │       │    starts     │
+    //                    │       │      └─────────────────┘       └───────────────┘                   │
+    //
+    //                    │       │                                                                    │
+    //
+    //                    │       │                                                                    │
+    //                             ◀──────────────────────────────────────────────────────────────────▶
+    //                    │                                  playToResult in                           │
+    //                                                     case of successful
+    //                    │                                     playback                               │
+    //
+    //                    │                                                                            │
+    //
+    //                    │◀──────────────────────────────────────────────────────────────────────────▶│
+    //                                                   clickToResult in
+    //                                                  case of successful
+    //                                                       playback
+    //
     [SRGDiagnosticsService serviceWithName:@"SRGPlaybackMetrics"].submissionBlock = ^(NSDictionary * _Nonnull JSONDictionary, void (^ _Nonnull completionBlock)(BOOL)) {
         NSString *diagnosticsServiceURLString = [[NSBundle srg_letterboxBundle] objectForInfoDictionaryKey:SRGLetterboxDiagnosticsServiceURLKey];
         NSURL *diagnosticsServiceURL = (diagnosticsServiceURLString.length > 0) ? [NSURL URLWithString:diagnosticsServiceURLString] : nil;
