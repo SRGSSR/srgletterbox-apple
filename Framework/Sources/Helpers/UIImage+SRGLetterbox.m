17f3e2e9e55576f58a8caef9c339ba6fa4428e75
Samuel Défago <defagos@gmail.com>
2019-08-07 09:47:07 +0200

    Remove unused method
    This method conflicts with the same implementation in SRG Appearance.

--- Framework/Sources/Helpers/UIImage+SRGLetterbox.m
+++ Framework/Sources/Helpers/UIImage+SRGLetterbox.m
@@ -198,30 +198,6 @@ static void SRGImageDrawPDFPageInRect(CGPDFPageRef pageRef, CGRect rect)
     return [self srg_vectorImageNamed:imageName inBundle:bundle withSize:size];
 }
 
-- (UIImage *)srg_imageTintedWithColor:(UIColor *)color
-{
-    if (! color) {
-        return self;
-    }
-    
-    CGRect rect = CGRectMake(0.f, 0.f, self.size.width, self.size.height);
-    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.f);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextTranslateCTM(context, 0.f, self.size.height);
-    CGContextScaleCTM(context, 1.0f, -1.0f);
-    
-    CGContextDrawImage(context, rect, self.CGImage);
-    CGContextSetBlendMode(context, kCGBlendModeSourceIn);
-    CGContextSetFillColorWithColor(context, color.CGColor);
-    CGContextFillRect(context, rect);
-    
-    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return tintedImage;
-}
-
 @end
 
 @implementation UIImage (SRGLetterboxImages)
