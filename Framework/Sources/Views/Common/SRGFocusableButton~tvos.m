bb39ee387d2f323f5e39c9af3e014ede118af9ee
Samuel Défago <defagos@gmail.com>
2019-10-28 07:41:25 +0100

    Respond to presses

--- Framework/Sources/Views/Common/SRGFocusableButton~tvos.m
+++ Framework/Sources/Views/Common/SRGFocusableButton~tvos.m
@@ -6,24 +6,95 @@
 
 #import "SRGFocusableButton.h"
 
+static CGFloat kFocusedScaleFactor = 1.2f;
+
+static void commonInit(SRGFocusableButton *self)
+{
+    // The focus effect alone is sufficient, no need for highlighting by default
+    self.adjustsImageWhenHighlighted = NO;
+}
+
 @implementation SRGFocusableButton
+
+#pragma mark Object lifecycle
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        commonInit(self);
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    if (self = [super initWithCoder:coder]) {
+        commonInit(self);
+    }
+    return self;
+}
 
 #pragma mark Overrides
 
 - (void)didUpdateFocusInContext:(UIFocusUpdateContext *)context withAnimationCoordinator:(UIFocusAnimationCoordinator *)coordinator
 {    
     [coordinator addCoordinatedAnimations:^{
-        if (context.nextFocusedView == self) {
-            [UIView animateWithDuration:UIView.inheritedAnimationDuration animations:^{
-                self.transform = CGAffineTransformMakeScale(1.2f, 1.2f);
-            }];
-        }
-        else if (context.previouslyFocusedView == self) {
-            [UIView animateWithDuration:UIView.inheritedAnimationDuration animations:^{
-                self.transform = CGAffineTransformIdentity;
-            }];
-        }
+        [UIView animateWithDuration:UIView.inheritedAnimationDuration animations:^{
+            [self updateAppearanceFocused:self.focused];
+        }];
     } completion:nil];
+}
+
+- (void)pressesBegan:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event
+{
+    [super pressesBegan:presses withEvent:event];
+    
+    if (presses.anyObject.type == UIPressTypeSelect) {
+        [UIView animateWithDuration:0.2 animations:^{
+            [self updateAppearanceFocused:NO];
+        } completion:nil];
+    }
+}
+
+- (void)pressesCancelled:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event
+{
+    [super pressesCancelled:presses withEvent:event];
+    
+    [self pressesFinished:presses withEvent:event];
+}
+
+- (void)pressesEnded:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event
+{
+    [super pressesEnded:presses withEvent:event];
+    
+    [self pressesFinished:presses withEvent:event];
+}
+
+- (void)pressesFinished:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event
+{
+    if (presses.anyObject.type == UIPressTypeSelect) {
+        [UIView animateWithDuration:0.2 animations:^{
+            [self updateAppearanceFocused:self.focused];
+        } completion:nil];
+    }
+}
+
+#pragma mark Appearance
+
+- (void)updateAppearanceFocused:(BOOL)focused
+{
+    if (focused) {
+        self.transform = CGAffineTransformMakeScale(kFocusedScaleFactor, kFocusedScaleFactor);
+        self.layer.shadowRadius = 10.f;
+        self.layer.shadowOpacity = 0.3f;
+        self.layer.shadowOffset = CGSizeMake(0.f, 20.f);
+    }
+    else {
+        self.transform = CGAffineTransformIdentity;
+        self.layer.shadowRadius = 0.f;
+        self.layer.shadowOpacity = 0.f;
+        self.layer.shadowOffset = CGSizeZero;
+    }
 }
 
 @end
