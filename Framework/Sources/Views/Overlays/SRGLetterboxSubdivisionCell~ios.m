fd6561524b1eabc9fe8826a9bc5731cba06d2737
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-12-16 11:55:39 +0100

    Unified live label translation

--- Framework/Sources/Views/Overlays/SRGLetterboxSubdivisionCell~ios.m
+++ Framework/Sources/Views/Overlays/SRGLetterboxSubdivisionCell~ios.m
@@ -98,7 +98,7 @@
         self.durationLabel.hidden = NO;
     }
     else if (subdivision.contentType == SRGContentTypeLivestream || subdivision.contentType == SRGContentTypeScheduledLivestream) {
-        NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:NSLocalizedString(@"Live", @"Short label identifying a livestream. Display in uppercase (uses the application translation).").uppercaseString
+        NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:SRGLetterboxLocalizedString(@"Live", @"Short label identifying a livestream. Display in uppercase.").uppercaseString
                                                                                            attributes:@{ NSFontAttributeName : [UIFont srg_mediumFontWithTextStyle:SRGAppearanceFontTextStyleCaption],
                                                                                                          NSForegroundColorAttributeName : UIColor.whiteColor }];
         
