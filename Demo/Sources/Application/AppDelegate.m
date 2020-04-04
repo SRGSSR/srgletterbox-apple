b3d49cee61f18e3ee62bd37ffca917105c66feec
Samuel Défago <defagos@gmail.com>
2020-04-04 13:33:04 +0200

    Enable private updates
    A private update track must be setup for updates to work since Appcenter 3.0

--- Demo/Sources/Application/AppDelegate.m
+++ Demo/Sources/Application/AppDelegate.m
@@ -144,6 +144,8 @@ static __attribute__((constructor)) void ApplicationInit(void)
         return YES;
     }];
     
+    MSDistribute.updateTrack = MSUpdateTrackPrivate;
+    
 #if TARGET_OS_IOS
     [MSAppCenter start:appCenterSecret withServices:@[ MSCrashes.class, MSDistribute.class ]];
 #else
