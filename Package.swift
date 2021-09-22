a4d8552cb7a553e88d967010874f12dff016ab67
Samuel Défago <defagos@gmail.com>
2021-09-22 14:44:06 +0200

    Update resolved dependencies

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     dependencies: [
         .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .exact("1.3.2-srg5")),
         .package(name: "OHHTTPStubs", url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")),
-        .package(name: "SRGAnalytics", url: "https://github.com/SRGSSR/srganalytics-apple.git", .branch("develop")),
-        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .upToNextMinor(from: "4.0.0")),
+        .package(name: "SRGAnalytics", url: "https://github.com/SRGSSR/srganalytics-apple.git", .upToNextMajor(from: "7.1.1")),
+        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .upToNextMajor(from: "4.0.1")),
         .package(name: "YYWebImage", url: "https://github.com/SRGSSR/YYWebImage.git", .exact("1.0.5-srg3"))
     ],
     targets: [
