272aebf22e642354885b700a25dff4d261de1d5f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-11-30 17:52:29 +0100

    Update SRG dependencies

--- Package.swift
+++ Package.swift
@@ -20,10 +20,9 @@ let package = Package(
         ) 
     ],
     dependencies: [
-        .package(name: "SRGMediaPlayer", url: "https://github.com/SRGSSR/srgmediaplayer-apple.git", .branch("feature/pip-opt-in")),
         .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .exact("1.3.2-srg5")),
         .package(name: "OHHTTPStubs", url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")),
-        .package(name: "SRGAnalytics", url: "https://github.com/SRGSSR/srganalytics-apple.git", .upToNextMinor(from: "7.3.0")),
+        .package(name: "SRGAnalytics", url: "https://github.com/SRGSSR/srganalytics-apple.git", .branch("develop")),
         .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .upToNextMinor(from: "5.0.0")),
         .package(name: "YYWebImage", url: "https://github.com/SRGSSR/YYWebImage.git", .exact("1.0.5-srg3"))
     ],
