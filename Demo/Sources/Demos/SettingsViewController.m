aba0688635a96f743432e013fc228b01721073ff
Samuel Défago <defagos@gmail.com>
2022-02-15 08:29:32 +0100

    Avoid interrupting focus animations

--- Demo/Sources/Demos/SettingsViewController.m
+++ Demo/Sources/Demos/SettingsViewController.m
@@ -883,7 +883,9 @@ NSDictionary<NSString *, NSString *> *ApplicationSettingGlobalParameters(void)
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    [self.tableView reloadData];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.tableView reloadData];
+    });
 }
 
 @end
