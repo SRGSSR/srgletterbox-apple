554461a9250c0f64e2069df798f7aa47216e627a
Samuel Défago <defagos@gmail.com>
2022-05-04 09:56:12 +0200

    Make animations a bit faster overall

--- Sources/SRGLetterbox/SRGLetterboxView~ios.m
+++ Sources/SRGLetterbox/SRGLetterboxView~ios.m
@@ -737,7 +737,7 @@ static const NSTimeInterval kDoubleTapDelay = 0.25;
     
     if (animated) {
         [self layoutIfNeeded];
-        [UIView animateWithDuration:0.2 animations:^{
+        [UIView animateWithDuration:0.15 animations:^{
             animations();
             [self layoutIfNeeded];
         } completion:completion];
