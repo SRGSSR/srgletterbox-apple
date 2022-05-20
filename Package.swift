db296506be650ae1189fa55080a525c087e53464
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-05-20 13:19:56 +0200

    Bump version number

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 struct ProjectSettings {
-    static let marketingVersion: String = "7.5.1"
+    static let marketingVersion: String = "8.0.0"
 }
 
 let package = Package(
